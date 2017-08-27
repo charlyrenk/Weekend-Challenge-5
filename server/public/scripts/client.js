116133d9b35ab2cc579b0017e107d55a8f0f6d93
Charles Renk <renkx008@umn.edu>
2017-08-27 17:09:04 -0500

    started to set up ng-routes to my two newly adde rentals.html and listings.html views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,14 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ng-route']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/views/home.html'
+    }).when('/listings', {
+        templateUrl: '/views/listings.html',
+        controller: 'listingsController as vm'
+        //or controlerAs: 'vm'
+    }).when('/rentals', {
+        templateUrl: '/views/rentals.html',
+        controller: 'rentalsController as vm'
+    })
+}]);
